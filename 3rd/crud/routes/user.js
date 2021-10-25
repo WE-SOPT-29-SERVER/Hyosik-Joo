cabc71edbd8a59bc84cd1cbec098db19beba7700
Hyosik Philip Joo <lux1937@gmail.com>
2021-10-25 15:53:39 +0900

    ✨ [FEAT] : Implement Update

--- 3rd/crud/routes/user.js
+++ 3rd/crud/routes/user.js
@@ -140,4 +140,36 @@ router.get("/profile/:id", async (req, res) => {
             )
     }
 })
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+    const {id} = req.params;
+    const {newName} = req.body;
+
+    if (!id || !newName) {
+        return res
+            .status(statusCode.BAD_REQUEST)
+            .send(
+                util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE)
+            );
+    }
+
+    const existingUser = users.filter(user => user.id === Number(id))[0];
+
+    if (!existingUser) {
+        return res
+            .status(statusCode.BAD_REQUEST)
+            .send(
+                util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER)
+            );
+    }
+
+    const updateUser = {...existingUser, name: newName}; // ... : 기존 객체 복사
+
+    res.status(statusCode.OK)
+        .send(
+            util.success(statusCode.OK, responseMessage.USER_UPDATE_SUCCESS, updateUser)
+        );
+});
+
 module.exports = router;
