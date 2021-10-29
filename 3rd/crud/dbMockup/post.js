8fdf397b7353c5a57bd2e118b3790af3188a23db
Hyosik Philip Joo <lux1937@gmail.com>
2021-10-30 00:13:33 +0900

    ✨ [FEAT] : Finish Assignment Level 2 - Posting Server

---
+++ 3rd/crud/dbMockup/post.js
@@ -0,0 +1,19 @@
+// dbMockup/post.js
+
+module.exports = [
+    {
+        id: 1,
+        title: "Node.js",
+        description: "Node is a Javascript runtime environment"
+    },
+    {
+        id: 2,
+        title: "Spring",
+        description: "Spring is a Java framework"
+    },
+    {
+        id: 3,
+        title: "AWS",
+        description: "Amazon Web Service is a cloud computing service"
+    },
+];
