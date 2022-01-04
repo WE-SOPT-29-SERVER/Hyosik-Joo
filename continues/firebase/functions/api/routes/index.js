aab6854151e3058e7a173dc43c0b29133abb0bee
Hyosik Philip Joo <lux1937@gmail.com>
2022-01-04 16:50:51 +0900

    ✨ [FEAT] : Test Open-Graph-Scraper

--- continues/firebase/functions/api/routes/index.js
+++ continues/firebase/functions/api/routes/index.js
@@ -4,4 +4,5 @@ const router = express.Router();
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
 router.use('/post', require('./post'));
+router.use('/scrap', require('./scrap'));
 module.exports = router;
